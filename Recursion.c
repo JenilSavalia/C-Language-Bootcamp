0190a7dbddf900940728f11ff1b480af7d50917e
Jenil Savalia <jenil.savalia.cd@gmail.com>
2024-10-09 17:33:45 +0530

    09-10-24

--- Recursion.c
+++ Recursion.c
@@ -2,68 +2,64 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-//int fact(int num){
-//	if(num==1){
-//		return 1;
-//	}
-//	else{
-//	    return num*fact(num-1);	
-//	}
-//
-//}
-//
-//
-//int main(){
-//	
-//int num = 5;
-//int upt = fact(num);
-//printf("%d",upt);
-//
-//}
-
-
-
-//int sum(int num){
-//	
-//		if(num>0){
-//		return num+sum(num-1);
-//		}
-//		else{
-//			return 0;
-//		}
-//}
-//
-//int main(){
-//	
-//int num = 2;
-//int upt = sum(num);
-//printf("%d",upt);
-//
-//}
-
-
-
-int fabb(int num){
+//1. Factorial of a Number
 	
-		if(num==0){
-		return 0;
-		}
-		else if(num==1){
-		return 1;
-		}
-		else{
-			return fabb(num-1) + fabb(num-2);
-			
-		}
+int factorial(int n) {
+    if (n == 0) {
+        return 1; // Base case: factorial of 0 is 1
+    }
+    return n * factorial(n - 1); // Recursive case
 }
 
-int main(){
-int num = 2;
-
-int upt = fabb(num);
-printf("%d",upt);
-
+int main() {
+    int num = 5;
+    printf("Factorial of %d is %d\n", num, factorial(num));
+    return 0;
 }
+
+
+//2. Fibonacci Series
+
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0; // Base case 1
+    } else if (n == 1) {
+        return 1; // Base case 2
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2); // Recursive case
+    }
+}
+
+int main() {
+    int num = 10;
+    for (int i = 0; i < num; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    return 0;
+}
+
+
+//3. Sum of Natural Numbers
+
+
+int sum(int n) {
+    if (n == 0) {
+        return 0; // Base case: sum of 0 is 0
+    }
+    return n + sum(n - 1); // Recursive case
+}
+
+int main() {
+    int num = 5;
+    printf("Sum of first %d natural numbers is %d\n", num, sum(num));
+    return 0;
+}
+
+
+
+
+
 
 
